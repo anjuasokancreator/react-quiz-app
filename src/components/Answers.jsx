7a32928313b73caf024bc53de9916a1a043702bb
Anju Asokan <Anju@vrize.com>
2026-01-09 11:56:07 +0530

    Initial commit - React quiz project

---
+++ src/components/Answers.jsx
@@ -0,0 +1,47 @@
+import { useRef } from "react";
+
+export default function Answers({
+  answers,
+  selectedAnswer,
+  answerState,
+  onSelect,
+}) {
+  const shuffledAnswers = useRef();
+
+  if (!shuffledAnswers.current) {
+    shuffledAnswers.current = [...answers];
+    shuffledAnswers.current.sort(() => Math.random() - 0.5);
+  }
+
+  return (
+    <ul id="answers">
+      {shuffledAnswers.current.map((answer) => {
+        const isSelected = selectedAnswer === answer;
+        let cssclass = "";
+
+        if (answerState === "answered" && isSelected) {
+          cssclass = "selected";
+        }
+
+        if (
+          (answerState === "correct" || answerState === "wrong") &&
+          isSelected
+        ) {
+          cssclass = answerState;
+        }
+
+        return (
+          <li key={answer} className="answer">
+            <button
+              disabled={answerState !== ""}
+              className={cssclass}
+              onClick={() => onSelect(answer)}
+            >
+              {answer}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
